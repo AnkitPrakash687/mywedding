2a5a33fa664ce6ca2252793b49b55f638e4a8a1f
Satish Gadge <ankit.prakash687@gmail.com>
2023-12-31 23:00:37 -0500

    update

--- components/InvitationVideo.vue
+++ components/InvitationVideo.vue
@@ -1,27 +1,33 @@
 <template>
+    <div @click="handleClickVideo">
+  <video id="vid" width="350" height="500" :autoplay="true" muted>
+    <source :src="InvitationVideo" type="video/mp4" />
+    Your browser does not support the video tag.
+  </video>
+</div>
   <div>
-    <img
-      v-if="displayGif"
-      height="500"
-      width="350"
-      src="@/public/assets/invitation.gif"
-    />
   </div>
 </template>
 
 <script>
-import InvitationVideo from '@/public/assets/invitationVideo.mp4';
+import InvitationVideo from "@/public/assets/invitationVideo.mp4"
 
+import InvitationGif from "@/public/assets/invitation.gif"
 export default {
-  mounted() {
-    this.displayGif = true;
-  },
-  data() {
-    return {
-      InvitationVideo,
-      displayGif: false,
-    };
-  },
+    mounted(){
+       // document.getElementById('vid').play();
+    },
+    methods: {
+        handleClickVideo() {
+            document.getElementById('vid').play();
+        }
+    },
+    data(){
+        return {
+            InvitationVideo,
+            InvitationGif
+        }
+    }
 };
 </script>
 
